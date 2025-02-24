a6070caf2408d8f1025d59a84737c14e977b1a0c
TIGER KILLER <choprakhushil13@gmail.com>
2025-02-24 11:55:52 +0530

    updated

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Your Name</h1>
+      <p>Email: your.email@example.com</p>
+      <p>Phone: +1234567890</p>
+    </div>
+  );
+}
+
+export default App;
